b61d2255079b04dd85e8eb225656b7c754d8da72
HOME-PC\Stelios <steliosdirects@gmail.com>
2021-01-31 18:19:51 +0200

    RoboApp 1st commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Mary Mertzani',
+      username: 'm2ertzani',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Stelios Alexandrakis',
+      username: 'kungfuPanda',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Matt Alexandrakis',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Maria Stratidaki',
+      username: 'abulafia',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Elias Alexandrakis',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'George Pilitsoglou',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'George vtof',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'theio leo',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'george agg',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'helena alex',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
